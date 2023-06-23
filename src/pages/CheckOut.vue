32263517f754875e54cc12986e979b182011e5dd
Valerio Salzano <vlr.slzn@gmail.com>
2023-06-23 20:18:47 +0200

    modifiche

--- src/pages/CheckOut.vue
+++ src/pages/CheckOut.vue
@@ -1,28 +1,41 @@
 <template>
+    <div class="container p-2 mt-3">
+        <h1 class="text-center mb-3">Riepilogo ordine</h1>
+        <ul class="list-group fw-semibold">
+            <li v-for="item in this.store.cart" class="list-group-item">{{ item.product.name }} x{{ item.product.quantity }}
+            </li>
+        </ul>
+        <p class="text-capitalize fw-semibold fs-5 m-3">totale {{ this.store.cartTotal }} &euro;</p>
+    </div>
 
-    <ul class="list-group">
-        <li v-for="item in this.store.cart" class="list-group-item">{{ item.product.name }}</li>
-    </ul>
-
-    <p>totale {{ this.store.cartTotal }}</p>
-
-    <input type="text" v-model="address">
-
-    <div v-if="isUserPaying">
+    <div v-if="isUserPaying" class="container">
+        <h2>Inserisci qui i tuoi dati</h2>
         <!-- Payment form fields -->
-        <input type="email" v-model="email" placeholder="Your email">
+        <div class="mb-3">
+            <label for="name" class="form-label">Nome</label>
+            <input type="text" class="form-control" id="name" placeholder="Inserisci qui il tuo nome">
+        </div>
+        <div class="mb-3">
+            <label for="Indirizzo" class="form-label">Indirizzo</label>
+            <input type="text" class="form-control" id="Indirizzo" placeholder="Inserisci l'indirizzo per la consegna">
+        </div>
+        <div class="mb-3">
+            <label for="phone" class="form-label">Num di telefono</label>
+            <input type="text" class="form-control" id="phone" placeholder="Inserisci il tuo recapito telefonico">
+        </div>
 
         <div id="dropin-container"></div>
         <!-- Submit button -->
-        <button id="submit-payment-btn" @click="sendForm()">Conferma pagamento</button>
+        <button id="submit-payment-btn" class="btn btn-warning w-100" @click="sendForm()">Conferma pagamento</button>
     </div>
-    <button v-else @click="getPaymentData">Paga con carta</button>
-
+    <div v-else class="container">
+        <button @click="getPaymentData" class="btn btn-warning w-100">Procedi con il pagamento</button>
+    </div>
 </template>
 
 <script>
 import axios from 'axios';
-import braintree from 'braintree-web';
+// import braintree from 'braintree-web';
 import { store } from '../store.js';
 import dropin from 'braintree-web-drop-in';
 
@@ -47,14 +60,14 @@ export default {
             return token;
         },
         emptyCart() {
-            
+
             this.store.cart = [];
             localStorage.setItem('cart', '');
-            
+
         },
         async getPaymentData() {
             // Create a client-side Braintree instance
-            const clientToken = await this.getToken(); 
+            const clientToken = await this.getToken();
 
             let that = this;
             if (clientToken) {
@@ -67,30 +80,36 @@ export default {
                     const submitBtn = document.getElementById('submit-payment-btn');
 
                     submitBtn.addEventListener('click', function () {
+                        this.classList.add('disabled');
                         dropinInstance.requestPaymentMethod(function (err, payload) {
+                            if(err){
+                                submitBtn.classList.remove('disabled');
+                            }
                             // Send the nonce to your Laravel backend for server-side processing
-                        
+
                             axios.post('http://localhost:8000/api/process-payment', {
                                 paymentMethodNonce: payload.nonce,
                                 total: that.store.cartTotal
-                            
+
                             }).then(response => {
 
-                                if(response.data.success == true) {
-    
+                                if (response.data.success == true) {
+
                                     that.emptyCart();
+                                }else{
+                                    submitBtn.classList.remove('disabled');
                                 }
 
-                                alert (response.data.message)
+                                alert(response.data.message)
 
                             })
-                                
-                                
+
+
                         })
                     })
                 })
 
-            
+
             } else {
                 console.log('Dropin creation failed: token not found');
             }
@@ -99,35 +118,35 @@ export default {
         // Invio dati al back per email di conferma
         sendForm() {
 
-            const payload  = {
-                    name: this.cardholderName,
-                    email: this.email,
-                    products: this.store.cart,
-                    price: this.store.total,
-                    address: this.address
-                    
-                }
-
-            axios.post(`${this.store.baseUrl}/api/neworder`, payload)
-            .then(response => { //200
-
-                if (response.data.success) {
-                    //resetto il form
-                    this.name = '';
-                    this.email = '';
-                    this.products = '';
-                    this.address = '';
-                    
-                } else {
-                    this.errors = response.data.errors;
-                    console.log(this.errors);
-                }
-
-                this.sending = false;
-
-            });
+            const payload = {
+                name: this.cardholderName,
+                email: this.email,
+                products: this.store.cart,
+                price: this.store.total,
+                address: this.address
 
             }
+
+            axios.post(`${this.store.baseUrl}/api/neworder`, payload)
+                .then(response => { //200
+
+                    if (response.data.success) {
+                        //resetto il form
+                        this.name = '';
+                        this.email = '';
+                        this.products = '';
+                        this.address = '';
+
+                    } else {
+                        this.errors = response.data.errors;
+                        console.log(this.errors);
+                    }
+
+                    this.sending = false;
+
+                });
+
+        }
     },
 }
 </script>
