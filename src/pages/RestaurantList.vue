7bd309dce7dc812a11e95a907c8f628818680515
Valerio Salzano <vlr.slzn@gmail.com>
2023-06-26 17:00:37 +0200

    finito alert

--- src/pages/RestaurantList.vue
+++ src/pages/RestaurantList.vue
@@ -9,6 +9,11 @@ export default {
     data() {
         return {
             store,
+            cartAlert: {
+                triggered: false,
+                slug: '',
+                id: '',
+            },
             products: []
         }
     },
@@ -27,12 +32,41 @@ export default {
                     console.log(response);
                 })
         },
-        emptyCart(id) {
-            if (this.store.lastVisitedRestaurantId != id) {
-                this.store.cart = [];
-                localStorage.setItem('cart', '');
+        emptyCart() {
+            this.store.cart = [];
+            localStorage.setItem('cart', '');
+        },
+        clickOnRestaurant(restaurant) {
+            //click su un ristorante diverso da quello già visitato
+            console.log('visito: ' + restaurant.id + '. Avevo visitato: ' + this.store.lastVisitedRestaurantId);
+            if (this.store.lastVisitedRestaurantId && this.store.lastVisitedRestaurantId != restaurant.id) {
+                //verifico se il carrello non è vuoto
+                if (this.store.cart.length) {
+                    //caso A: ALERT, prodotti nel carrello
+                    this.cartAlert.triggered = true;
+                    this.cartAlert.slug = restaurant.slug;
+                } else {
+                    //caso B: procedo al ristorante
+                    this.goToRestaurant(restaurant.id, restaurant.slug)
+                }
+            } else {
+                //caso B: procedo al ristorante
+                this.goToRestaurant(restaurant.id, restaurant.slug)
             }
+
+        },
+        clickOnAlertBtn(input) {
+            if (input == 'close') {
+
+                this.cartAlert.triggered = false;
+            } else if (input == 'confirm') {
+                this.emptyCart();
+                this.goToRestaurant(this.cartAlert.id, this.cartAlert.slug)
+            }
+        },
+        goToRestaurant(id, slug) {
             this.store.lastVisitedRestaurantId = id;
+            this.$router.push({ name: 'single-restaurant', params: { slug } });
         }
     },
     watch: {
@@ -41,12 +75,13 @@ export default {
                 .then(response => {
                     this.store.restaurantList = response.data.results;
                 })
-        }
+        },
     },
     created() {
         this.getRestaurantList(this.$route.query.types);
     }
 }
+
 </script>
 
 
@@ -55,19 +90,32 @@ export default {
     <div class="container p-3">
         <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
             <div class="col" v-for="restaurant in this.store.restaurantList">
-                <div class="card text-center h-100" >
+                <div class="card text-center h-100">
                     <div class="card-body">
                         <h5 class="card-title">{{ restaurant.name }}</h5>
                     </div>
                     <div class="card-footer">
-                        <router-link :to="{ name: 'single-restaurant', params: { slug: restaurant.slug } }"
-                            class="btn btn-warning" @click="emptyCart(restaurant.id)">
+                        <button class="btn btn-warning show-restaurant-btn" @click="clickOnRestaurant(restaurant)">
                             Ordina da qui
-                        </router-link>
+                        </button>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
+    <div v-if="cartAlert.triggered"
+        class="position-fixed bottom-50 start-50 translate-middle p-5 border border-dark rounded text-bg-light">
+        <p>Hai dei prodotti nel carrello, se prosegui verranno eliminati.</p>
+        <div class="d-flex justify-content-between px-3">
+            <button class="btn btn-warning" @click="clickOnAlertBtn('confirm')">
+                Procedi
+            </button>
+            <button class="btn btn-secondary" @click="clickOnAlertBtn('close')">
+                Torna alla lista
+            </button>
+        </div>
+
+        <button class="btn btn-close position-absolute top-0 end-0 m-3" @click="clickOnAlertBtn('close')"></button>
     </div>
 </template>
 
