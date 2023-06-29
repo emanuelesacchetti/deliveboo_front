22769b0ba0d7c65fbf2f29d6acc198204e6b0861
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-06-29 12:32:02 +0200

    button and responsive chackout

--- src/pages/CheckOut.vue
+++ src/pages/CheckOut.vue
@@ -1,39 +1,51 @@
 <template>
-    <div class="container p-2 mt-3">
-        <h1 class="text-center mb-3">Riepilogo ordine</h1>
-        <ul class="list-group fw-semibold">
-            <li v-for="item in this.store.cart" class="list-group-item">{{ item.product.name }} x{{ item.product.quantity }}
-            </li>
-        </ul>
-        <p class="text-capitalize fw-semibold fs-5 m-3">totale {{ this.store.cartTotal }} &euro;</p>
-    </div>
-
-    <div v-if="isUserPaying" class="container">
-        <h2>Inserisci qui i tuoi dati</h2>
-        <!-- Payment form fields -->
-        <div class="mb-3">
-            <label for="name" class="form-label">Nome</label>
-            <input v-model="orderPayload.name" type="text" class="form-control" id="name" placeholder="Inserisci qui il tuo nome">
-        </div>
-        <div class="mb-3">
-            <label for="Indirizzo" class="form-label">Indirizzo</label>
-            <input v-model="orderPayload.address" type="text" class="form-control" id="Indirizzo" placeholder="Inserisci l'indirizzo per la consegna">
-        </div>
-        <div class="mb-3">
-            <label for="email" class="form-label">E-mail</label>
-            <input v-model="orderPayload.email" type="email" class="form-control" id="email" placeholder="Inserisci la tua email">
-        </div>
-        <div class="mb-3">
-            <label for="phone" class="form-label">Phone</label>
-            <input v-model="orderPayload.phone_number" type="email" class="form-control" id="phone" placeholder="Inserisci il tuo recapito">
+    <div class="container_wrapper">
+        <div class="container p-2">
+            <h1 class="text-center mb-3">Riepilogo ordine</h1>
+            <ul class="list-group fw-semibold">
+                <li v-for="item in this.store.cart" class="list-group-item">{{ item.product.name }} x{{
+                    item.product.quantity }}
+                </li>
+            </ul>
+            <p class="text-capitalize fw-semibold fs-5 m-3">totale {{ this.store.cartTotal }} &euro;</p>
         </div>
 
-        <div id="dropin-container"></div>
-        <!-- Submit button -->
-        <button id="submit-payment-btn" class="btn btn-warning w-100">Conferma pagamento</button>
-    </div>
-    <div v-else class="container">
-        <button @click="getPaymentData" class="btn btn-warning w-100" :class="{'disabled':!store.cart.length}">Procedi con il pagamento</button>
+        <div v-if="isUserPaying" class="container ">
+            <h2 class="text-center">Inserisci qui i tuoi dati</h2>
+            <!-- Payment form fields -->
+            <div class="mb-3">
+                <label for="name" class="form-label">Nome</label>
+                <input v-model="orderPayload.name" type="text" class="form-control" id="name"
+                    placeholder="Inserisci qui il tuo nome">
+            </div>
+            <div class="mb-3">
+                <label for="Indirizzo" class="form-label">Indirizzo</label>
+                <input v-model="orderPayload.address" type="text" class="form-control" id="Indirizzo"
+                    placeholder="Inserisci l'indirizzo per la consegna">
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">E-mail</label>
+                <input v-model="orderPayload.email" type="email" class="form-control" id="email"
+                    placeholder="Inserisci la tua email">
+            </div>
+            <div class="mb-3">
+                <label for="phone" class="form-label">Phone</label>
+                <input v-model="orderPayload.phone_number" type="email" class="form-control" id="phone"
+                    placeholder="Inserisci il tuo recapito">
+            </div>
+
+            <div id="dropin-container"></div>
+            <!-- Submit button -->
+            <div class="container_button">
+                <button id="submit-payment-btn">Conferma pagamento</button>
+            </div>
+
+        </div>
+        <div v-else class="container_button">
+            <button @click="getPaymentData" :class="{ 'disabled': !store.cart.length }">Procedi
+                con
+                il pagamento</button>
+        </div>
     </div>
 </template>
 
@@ -60,9 +72,9 @@ export default {
         }
     },
     methods: {
-        getProducts(){
+        getProducts() {
             let products = [];
-            this.store.cart.forEach( item => {
+            this.store.cart.forEach(item => {
                 let product = {
                     id: item.product.id,
                     quantity: item.product.quantity
@@ -71,8 +83,8 @@ export default {
             })
             this.orderPayload.products = products;
         },
-        getRestaurantId(){
-            if(store.cart.length){
+        getRestaurantId() {
+            if (store.cart.length) {
                 //da provare this.orderPayload.restaurant_id = this.store.lastVisitedRestaurantId;
                 this.orderPayload.restaurant_id = store.cart[0].product.restaurant_id;
             }
@@ -108,7 +120,7 @@ export default {
                     submitBtn.addEventListener('click', function () {
                         this.classList.add('disabled');
                         dropinInstance.requestPaymentMethod(function (err, payload) {
-                            if(err){
+                            if (err) {
                                 submitBtn.classList.remove('disabled');
                             }
                             // Send the nonce to your Laravel backend for server-side processing
@@ -120,8 +132,8 @@ export default {
 
                                 if (response.data.success == true) {
                                     that.emptyCart();
-                                    that.$router.push({ name: 'checkout-success', params: { orderCode: response.data.orderCode }});
-                                }else{
+                                    that.$router.push({ name: 'checkout-success', params: { orderCode: response.data.orderCode } });
+                                } else {
                                     submitBtn.classList.remove('disabled');
                                 }
                             })
@@ -138,9 +150,38 @@ export default {
             // this.isDropinLoading = false;
         },
     },
-    mounted(){
+    mounted() {
         this.getProducts();
         this.getRestaurantId();
     }
 }
 </script>
+
+<style lang="scss" scoped>
+@use '../partials/variables.scss' as *;
+@use '../partials/mixins.scss';
+
+.container_wrapper {
+    margin-top: 40vh;
+    margin-bottom: 40vh;
+}
+
+button {
+    @include mixins.primary-btn;
+    border-color: $secondary-color;
+}
+
+button:hover {
+    @include mixins.btn-hover;
+    font-size: 12px;
+}
+
+button:active {
+    @include mixins.btn-active;
+}
+
+.container_button {
+    display: flex;
+    justify-content: center;
+}
+</style>
