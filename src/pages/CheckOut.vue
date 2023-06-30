cdbb1b90a8ef65ee5d57311dd2d588d71d486942
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-06-30 12:31:18 +0200

    fix button

--- src/pages/CheckOut.vue
+++ src/pages/CheckOut.vue
@@ -9,69 +9,78 @@
             </ul>
             <p class="text-capitalize fw-semibold fs-5 m-3">totale {{ this.store.cartTotal }} &euro;</p>
         </div>
-    
 
-    <div v-if="isUserPaying" class="container">
-        <h2>Inserisci qui i tuoi dati</h2>
-        <!-- Payment form fields -->
-        <div v-show="errors" class="text-danger">
-                <ul v-for="error,index in errors ">
+
+        <div v-if="isUserPaying" class="container">
+            <h2>Inserisci qui i tuoi dati</h2>
+            <!-- Payment form fields -->
+            <div v-show="errors" class="text-danger">
+                <ul v-for="error, index in errors ">
                     <li>{{ error[0] }}</li>
                 </ul>
-        </div>
-        
-        <!-- Inserimento nome -->
-        <div class="mb-3">
-            <label for="name" class="form-label">Nome</label>
-            <input v-model="orderPayload.name" type="text" class="form-control" id="name"  placeholder="Inserisci qui il tuo nome">
-            <div v-show="!orderPayload.name && buttonClicked" class="text-danger">
-                Il campo nome è richiesto
+            </div>
+
+            <!-- Inserimento nome -->
+            <div class="mb-3">
+                <label for="name" class="form-label">Nome</label>
+                <input v-model="orderPayload.name" type="text" class="form-control" id="name"
+                    placeholder="Inserisci qui il tuo nome">
+                <div v-show="!orderPayload.name && buttonClicked" class="text-danger">
+                    Il campo nome è richiesto
+                </div>
+            </div>
+
+            <!-- Inserimento Indirizzo -->
+            <div class="mb-3">
+                <label for="Indirizzo" class="form-label">Indirizzo</label>
+                <input v-model="orderPayload.address" type="text" class="form-control" id="Indirizzo"
+                    placeholder="Inserisci l'indirizzo per la consegna">
+                <div v-show="!orderPayload.address && buttonClicked" class="text-danger">
+                    L'indirizzo è richiesto
+                </div>
+            </div>
+
+            <!-- Inserimento E-mail -->
+            <div class="mb-3">
+                <label for="email" class="form-label">E-mail</label>
+                <input v-model="orderPayload.email" type="email" class="form-control" id="email"
+                    placeholder="Inserisci la tua email">
+                <div v-show="!orderPayload.email && buttonClicked" class="text-danger">
+                    L'E-mail è richiesta
+                </div>
+                <div v-show="(!orderPayload.email.match(/@[^.]*\.(?:com|it)\b/gm)) && buttonClicked" class="text-danger">
+                    L'E-mail non è valida
+                </div>
+            </div>
+
+            <!-- Inserimento telefono -->
+            <div class="mb-3">
+                <label for="phone" class="form-label">Phone</label>
+                <input v-model="orderPayload.phone_number" type="number" class="form-control" id="phone"
+                    placeholder="Inserisci il tuo recapito">
+
+                <div v-show="!orderPayload.phone_number && buttonClicked" class="text-danger">
+                    Il contatto telefonico è richiesto
+                </div>
+                <div v-show="((orderPayload.phone_number > 9999999999) || (orderPayload.phone_number < 999999999)) && buttonClicked"
+                    class="text-danger">
+                    Il campo può contenere solo 10 cifre
+                </div>
+            </div>
+
+
+            <div id="dropin-container"></div>
+            <!-- Submit button -->
+            <div class="d-flex justify-content-center">
+                <button id="submit-payment-btn" class="d-flex " @click="buttonClicked = true"
+                    :class="{ 'disabled': checkFormValidity }">Conferma pagamento</button>
             </div>
         </div>
 
-        <!-- Inserimento Indirizzo -->
-        <div class="mb-3">
-            <label for="Indirizzo" class="form-label">Indirizzo</label>
-            <input v-model="orderPayload.address" type="text" class="form-control" id="Indirizzo"  placeholder="Inserisci l'indirizzo per la consegna">
-            <div v-show="!orderPayload.address && buttonClicked" class="text-danger">
-                L'indirizzo è richiesto
-            </div>
+        <div v-else class="container d-flex justify-content-center">
+            <button @click="getPaymentData">Procedi con il pagamento</button>
         </div>
-
-        <!-- Inserimento E-mail -->
-        <div class="mb-3">
-            <label for="email" class="form-label">E-mail</label>
-            <input v-model="orderPayload.email" type="email" class="form-control" id="email"  placeholder="Inserisci la tua email">
-            <div v-show="!orderPayload.email && buttonClicked" class="text-danger">
-                L'E-mail è richiesta
-            </div>
-            <div v-show="(!orderPayload.email.match(/@[^.]*\.(?:com|it)\b/gm)) && buttonClicked" class="text-danger">
-                L'E-mail non è valida
-            </div>
-        </div>
-
-        <!-- Inserimento telefono -->
-        <div class="mb-3">
-            <label for="phone" class="form-label">Phone</label>
-            <input v-model="orderPayload.phone_number" type="number" class="form-control" id="phone" placeholder="Inserisci il tuo recapito">
-
-            <div v-show="!orderPayload.phone_number && buttonClicked" class="text-danger">
-                Il contatto telefonico è richiesto
-            </div>
-            <div v-show="((orderPayload.phone_number > 9999999999) || (orderPayload.phone_number < 999999999)) && buttonClicked" class="text-danger">
-                Il campo può contenere solo 10 cifre
-            </div>
-        </div>
-
-
-        <div id="dropin-container"></div>
-        <!-- Submit button -->
-        <button id="submit-payment-btn" class="btn btn-warning w-100" @click="buttonClicked = true" :class="{ 'disabled': checkFormValidity }">Conferma pagamento</button>
     </div>
-    <div v-else class="container">
-        <button @click="getPaymentData" class="btn btn-warning w-100">Procedi con il pagamento</button>
-    </div>
-</div>
 </template>
 
 <script>
@@ -100,9 +109,9 @@ export default {
         }
     },
     methods: {
-        getProducts(){
+        getProducts() {
             let products = [];
-            this.store.cart.forEach( item => {
+            this.store.cart.forEach(item => {
                 let product = {
                     id: item.product.id,
                     quantity: item.product.quantity
@@ -111,8 +120,8 @@ export default {
             })
             this.orderPayload.products = products;
         },
-        getRestaurantId(){
-            if(store.cart.length){
+        getRestaurantId() {
+            if (store.cart.length) {
                 this.orderPayload.restaurant_id = store.cart[0].product.restaurant_id;
             }
         },
@@ -147,7 +156,7 @@ export default {
                     submitBtn.addEventListener('click', function () {
                         this.classList.add('disabled');
                         dropinInstance.requestPaymentMethod(function (err, payload) {
-                            if(err){
+                            if (err) {
                                 submitBtn.classList.remove('disabled');
                             }
                             // Send the nonce to your Laravel backend for server-side processing
@@ -159,8 +168,8 @@ export default {
 
                                 if (response.data.success == true) {
                                     that.emptyCart();
-                                    that.$router.push({ name: 'checkout-success', params: { orderCode: response.data.orderCode }});
-                                }else{
+                                    that.$router.push({ name: 'checkout-success', params: { orderCode: response.data.orderCode } });
+                                } else {
                                     submitBtn.classList.remove('disabled');
                                 }
 
@@ -178,29 +187,28 @@ export default {
             this.isDropinLoading = false;
         },
     },
-    computed : {
+    computed: {
 
         checkFormValidity() {
 
-        if(this.buttonClicked) {
+            if (this.buttonClicked) {
 
-            if((this.orderPayload.name) && 
-                (this.orderPayload.email)&&
-                (this.orderPayload.address)&&
-                (this.orderPayload.phone_number) &&
-                (this.orderPayload.email.match(/@[^.]*\.(?:com|it)\b/gm)) &&
-                (this.orderPayload.phone_number.match(/[^0-9]/g))
-                ) 
-            {
-                return '';
-            } else {
-                return 'disabled';
+                if ((this.orderPayload.name) &&
+                    (this.orderPayload.email) &&
+                    (this.orderPayload.address) &&
+                    (this.orderPayload.phone_number) &&
+                    (this.orderPayload.email.match(/@[^.]*\.(?:com|it)\b/gm)) &&
+                    (this.orderPayload.phone_number.match(/[^0-9]/g))
+                ) {
+                    return '';
+                } else {
+                    return 'disabled';
+                }
+
             }
 
-        }
 
-            
-    }
+        }
 
     },
     mounted() {
